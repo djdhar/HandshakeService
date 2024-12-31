77fe3bfe16986ffffaf2f46a728b874e7dae5539
Dibyajyoti Dhar <dibyajyotidhar@gmail.com>
2024-12-31 23:59:40 +0530

    adding docker files

--- makehandshake/makehandshake/src/main/java/com/project/makehandshake/config/KafkaOverallConfig.java
+++ makehandshake/makehandshake/src/main/java/com/project/makehandshake/config/KafkaOverallConfig.java
@@ -1,15 +1,16 @@
 package com.project.makehandshake.config;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.ContainerProperties;
 
 import java.util.HashMap;
@@ -17,12 +18,18 @@ import java.util.Map;
 
 @Configuration
 @EnableKafka
-public class KafkaProducerConfig {
+public class KafkaOverallConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaServer;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String consumerGroup;
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
@@ -39,8 +46,8 @@ public class KafkaProducerConfig {
         ConcurrentKafkaListenerContainerFactory<String, String> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory);
-        factory.setBatchListener(true); // Enable batch mode
-        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL); // Enable manual acknowledgment
+        factory.setBatchListener(true); // Enable batch processing
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL); // Manual acknowledgment
         return factory;
     }
 }
--- makehandshake/makehandshake/src/main/java/com/project/makehandshake/controller/MakeHandshakeController.java
+++ makehandshake/makehandshake/src/main/java/com/project/makehandshake/controller/MakeHandshakeController.java
@@ -4,6 +4,7 @@ import com.project.makehandshake.model.HandShake;
 import com.project.makehandshake.model.HandShakeResponse;
 import com.project.makehandshake.service.KafkaConsumerService;
 import com.project.makehandshake.service.MakeHandshakeServ;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,11 @@ public class MakeHandshakeController {
     @PostMapping("/v1/create_handshake")
     public Mono<HandShakeResponse> createHandshake(@Validated @RequestBody HandShake handShake) {
         return makeHandshakeServ.makeHandshakeService(handShake);
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Service is healthy!");
     }
 
     @GetMapping(value = "/v1/stream_admin_handshake", produces = "text/event-stream")
